0e9f413bd457bb70fb949c65b91826e9f02115d0
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-12-11 18:30:26 +0100

    add the visualization function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import streamlit.components.v1 as components
+from ipywidgets import embed
+import vtk
+from itkwidgets import view
+
+path_to_file = './samples/segmentation_liver.nii.gz'
 
 st.set_page_config(page_title='3D Visualization', page_icon=':pill:', layout='wide')
 
@@ -6,3 +12,16 @@ st.subheader('This is Amine :wave:')
 st.title("Data Scientist")
 st.write("I am a phd student")
 st.write("[Visit my Website](https://pycad.co/)")
+
+if st.button('Show 3D'):
+    with st.container():
+        reader = vtk.vtkNIFTIImageReader()
+        reader.SetFileName(path_to_file)
+        reader.Update()
+
+        view_width = 800
+        view_height = 600
+
+        snippet = embed.embed_snippet(views=view(reader.GetOutput()))
+        html = embed.html_template.format(title="", snippet=snippet)
+        components.html(html, width=view_width, height=view_height)

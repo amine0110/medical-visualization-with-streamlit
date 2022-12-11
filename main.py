fbf81f94bcc60ab397f06d4f6153681fbfa61684
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-12-11 19:39:25 +0100

    added the lotties

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import streamlit.components.v1 as components
 from ipywidgets import embed
 import vtk
 from itkwidgets import view
+from streamlit_lottie import st_lottie
+import requests
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 path_to_file = './samples/volume.nii.gz'
+lottie_file = load_lottieurl('https://assets10.lottiefiles.com/private_files/lf30_4FGi6N.json')
 
 st.set_page_config(page_title='3D Visualization', page_icon=':pill:', layout='wide')
 
@@ -12,6 +21,8 @@ st.subheader('This is Amine :wave:')
 st.title("Data Scientist")
 st.write("I am a phd student")
 st.write("[Visit my Website](https://pycad.co/)")
+
+st_lottie(lottie_file, height=1000, key='coding')
 
 if st.button('Show 3D'):
     with st.container():
@@ -25,3 +36,4 @@ if st.button('Show 3D'):
         snippet = embed.embed_snippet(views=view(reader.GetOutput()))
         html = embed.html_template.format(title="", snippet=snippet)
         components.html(html, width=view_width, height=view_height)
+

e071669eb2e01a9a376ced1965172d8999aee2cc
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-12-11 18:20:13 +0100

    create the base UI

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title='3D Visualization', page_icon=':pill:', layout='wide')
+
+st.subheader('This is Amine :wave:')
+st.title("Data Scientist")
+st.write("I am a phd student")
+st.write("[Visit my Website](https://pycad.co/)")

7ae6bf25d102f7cfdd568cada4e5d71f39f5f145
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2022-12-11 19:40:27 +0100

    added contact form

--- main.py
+++ main.py
@@ -37,3 +37,23 @@ if st.button('Show 3D'):
         html = embed.html_template.format(title="", snippet=snippet)
         components.html(html, width=view_width, height=view_height)
 
+with st.container():
+        st.write("---")
+        st.header("Get In Touch With Us!")
+        st.write("##")
+
+        # Documention: https://formsubmit.co/ 
+        contact_form = """
+        <form action="https://formsubmit.co/mohammed@velmeni.com" method="POST">
+            <input type="hidden" name="_captcha" value="false">
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="email" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Your message here" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+        """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
